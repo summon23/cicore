118a22914d8186603f5ac3befa11b6e52c6abd69
Andry <andreymahdison@gmail.com>
2018-11-25 22:50:10 +0700

    add pdf library

--- application/libraries/Pdf.php
+++ application/libraries/Pdf.php
@@ -1,5 +1,13 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+require 'vendor/autoload.php';
+use Dompdf\Dompdf;
 class Pdf {
-    
+    /**
+     * @return DOMPdf Object
+     */
+    public function getContext()
+    {
+        return new Dompdf();
+    }
 }
